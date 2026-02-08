3c9c6440f31c8aae0baa66b46f679af10117b08d
JP Jantiwa <jantiwaphron@gmail.com>
2026-02-09 06:07:02 +0700

    fix: vite alias for ESM (__dirname replacement)

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || "/",
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src",
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
